bda6940c773505c2cdc56f40d1e08a839441107f
suhaib <suhaib.build@gmail.com>
2026-05-01 17:21:47 +0500

    desgined the structure

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+
+
+const newStartButton = document.querySelector("#btns")
+
+newStartButton.addEventListener("click", function () {
+  const newStartForm = document.querySelector("#form-new")
+  newStartForm.style.display = "block";
+
+})
